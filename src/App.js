e532f35004561cfee71c62412395b1f86694cdaa
Logan Leopold <loganleopld@me.com>
2019-03-12 15:42:37 -0400

    building bulk of app view with forms for trying to send data to api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React, { Component } from 'react';
-import {Link, Route} from 'react-router-dom'
-import Tester from './Tester'
-import './App.css';
+import React, { Component } from "react";
+import { Link, Route } from "react-router-dom";
+import Tester from "./Tester";
+import "./App.css";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      test: []
-    }
+      inboundDate: "",
+      cabinClass: "",
+      originPlace: "",
+      destinationPlace: "",
+      outboundDate: "",
+      adults: ""
+    };
   }
 
-  componentDidMount() {
-    console.log('App mounted yo')
+  handleChange(e) {
+      const name = e.target.name
+      const value = e.target.value
+    this.setState({
+      [name]: value
+    });
   }
 
   render() {
-    console.log('App rendered boi')
+    console.log("App rendered boi");
     return (
-      <main>
-      <Route path='/tester' exact render={(routerProps) => <Tester {...routerProps}{...this.state}/>} />
-      </main>
+      <div>
+        <main>
+          <label>Flight Date</label>
+          <input
+            type="text"
+            name="inboundDate"
+            defaultValue={this.state.inboundDate}
+            onChange={this.handleChange}
+          />
+          <label>Title</label>
+          <input
+            type="text"
+            name="cabinClass"
+            defaultValue={this.state.cabinClass}
+            onChange={this.handleChange}
+          />
+          <label>Title</label>
+          <input
+            type="text"
+            name="originPlace"
+            defaultValue={this.state.originPlace}
+            onChange={this.handleChange}
+          />
+          <label>Title</label>
+          <input
+            type="text"
+            name="destinationPlace"
+            defaultValue={this.state.destinationPlace}
+            onChange={this.handleChange}
+          />
+          <label>Title</label>
+          <input
+            type="text"
+            name="outboundDate"
+            defaultValue={this.state.outboundDate}
+            onChange={this.handleChange}
+          />
+          <label>Title</label>
+          <input
+            type="text"
+            name="adults"
+            defaultValue={this.state.adults}
+            onChange={this.handleChange}
+          />
+        </main>
+      </div>
     );
   }
 }
