0f3b9c4c03fed9c4684057b46472eafa28bd4b33
Logan Leopold <loganleopld@me.com>
2020-03-24 23:48:46 -0400

    got login.js updating state with type in inputs

---
+++ src/CreateUser.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CreateUser extends Component {
+    render() {
+        return (
+            <div>
+                <h1>No account? Create a user here.</h1>
+            </div>
+        );
+    }
+}
+
+export default CreateUser;
--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            user: '',
+            password: ''
+        };
+        this.handleChange = this.handleChange.bind(this)
+      }
+
+      handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
+      }
+
     render() {
         return (
             <div>
-                <h1>Hello login world!</h1>
+                <div>
+                    <h1>Hello login world!</h1>
+                </div>
+                <form>
+                    <label>User</label>
+                    <input
+                        type="text"
+                        name="user"
+                        defaultValue={this.state.user}
+                        onChange={this.handleChange}
+                    />
+                    <label>Password</label>
+                    <input
+                        type="text"
+                        name="password"
+                        defaultValue=''
+                        onChange={this.handleChange}
+                    />
+                </form>
             </div>
         );
     }
